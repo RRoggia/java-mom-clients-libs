59ca73907035e0990414eb3dbcad3130eb48ef7f
RRoggia <renanzr@gmail.com>
2019-09-11 13:35:12 -0300

    add mom activemq folder

---
+++ mom-activemq-classic/src/src/main/java/com/rroggia/activemq/p2p/Application.java
@@ -0,0 +1,30 @@
+package com.rroggia.activemq.p2p;
+
+import javax.jms.Connection;
+import javax.jms.ConnectionFactory;
+import javax.jms.JMSException;
+
+import org.apache.activemq.ActiveMQConnectionFactory;
+
+public class Application {
+	public static void main(String[] args) {
+		ConnectionFactory connectionFactory = new ActiveMQConnectionFactory();
+
+		try {
+			Connection connection = connectionFactory.createConnection();
+
+			Ping ping = new Ping(connection);
+			new Pong(connection);
+
+			connection.start();
+
+			ping.producesToPong(connection);
+
+		} catch (JMSException e) {
+			System.out.println("Unable to create connection");
+			System.out.println("Error message " + e.getMessage());
+			e.printStackTrace();
+		}
+
+	}
+}
