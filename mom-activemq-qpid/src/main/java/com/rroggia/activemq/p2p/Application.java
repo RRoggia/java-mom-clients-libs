9ad8c4289f31c13295bc37fc632fb7bd3d7ba51c
RRoggia <renanzr@gmail.com>
2019-09-12 17:17:47 -0300

    add example using qpid jms

--- mom-activemq-qpid/src/main/java/com/rroggia/activemq/p2p/Application.java
+++ mom-activemq-qpid/src/main/java/com/rroggia/activemq/p2p/Application.java
@@ -1,5 +1,32 @@
 package com.rroggia.activemq.p2p;
 
+import javax.jms.Connection;
+import javax.jms.ConnectionFactory;
+import javax.jms.JMSException;
+
+import org.apache.qpid.jms.JmsConnectionFactory;
+
 public class Application {
+	public static void main(String[] args) {
+
+		ConnectionFactory connectionFactory = new JmsConnectionFactory();
+
+		try {
+			Connection connection = connectionFactory.createConnection();
+
+			Ping ping = new Ping(connection);
+			new Pong(connection);
+
+			connection.start();
+
+			ping.producesToPong(connection);
+
+		} catch (JMSException e) {
+			System.out.println("Unable to create connection");
+			System.out.println("Error message " + e.getMessage());
+			e.printStackTrace();
+		}
+
+	}
 
 }
