8f3e40f2e9c382e47e8b58b3aa125d38a788674a
RRoggia <renanzr@gmail.com>
2019-09-12 19:56:10 -0300

    add missing component tag

--- mom-activemq-spring-jms/src/main/java/com/rroggia/activemq/p2p/Ping.java
+++ mom-activemq-spring-jms/src/main/java/com/rroggia/activemq/p2p/Ping.java
@@ -1,16 +1,25 @@
 package com.rroggia.activemq.p2p;
 
+import javax.annotation.PostConstruct;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jms.annotation.JmsListener;
 import org.springframework.jms.core.JmsTemplate;
+import org.springframework.stereotype.Component;
 
+@Component
 public class Ping {
 
 	@Autowired
 	private JmsTemplate jmsTemplate;
 
+	@PostConstruct
+	public void init() {
+		listenPingQueue();
+	}
+
 	@JmsListener(destination = "p2p-ping")
-	public void listenPingQueue(String textMessage) {
+	public void listenPingQueue() {
 		System.out.println("ping");
 		jmsTemplate.send("p2p-pong", session -> session.createMessage());
 	}
--- mom-activemq-spring-jms/src/main/java/com/rroggia/activemq/p2p/Pong.java
+++ mom-activemq-spring-jms/src/main/java/com/rroggia/activemq/p2p/Pong.java
@@ -3,14 +3,16 @@ package com.rroggia.activemq.p2p;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jms.annotation.JmsListener;
 import org.springframework.jms.core.JmsTemplate;
+import org.springframework.stereotype.Component;
 
+@Component
 public class Pong {
 
 	@Autowired
 	private JmsTemplate jmsTemplate;
 
 	@JmsListener(destination = "p2p-pong")
-	public void listenPingQueue(String textMessage) {
+	public void listenPingQueue() {
 		System.out.println("pong");
 		jmsTemplate.send("p2p-ping", session -> session.createMessage());
 	}
